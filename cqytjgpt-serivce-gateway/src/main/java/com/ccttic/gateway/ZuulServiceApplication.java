383506400ba8e852045ac31dac1d61ecd296aebc
axzsd <atva725@qq.com>
2018-05-18 09:18:23 +0800

    wl xx

--- cqytjgpt-serivce-gateway/src/main/java/com/ccttic/gateway/ZuulServiceApplication.java
+++ cqytjgpt-serivce-gateway/src/main/java/com/ccttic/gateway/ZuulServiceApplication.java
@@ -6,13 +6,32 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 public class ZuulServiceApplication {
     public static void main(String [] args) {
         ConfigurableApplicationContext context = new SpringApplicationBuilder(ZuulServiceApplication.class).web(true).run(args);
+    }
+
+    /**
+     * 说明：解决zuul跨域问题，只在zuul解决就成
+     * */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 允许cookies跨域
+        config.addAllowedOrigin("*");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+        config.addAllowedHeader("*");// #允许访问的头信息,*表示全部
+        config.setMaxAge(18000L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+        config.addAllowedMethod("*");// 允许提交请求的方法，*表示全部允许
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
