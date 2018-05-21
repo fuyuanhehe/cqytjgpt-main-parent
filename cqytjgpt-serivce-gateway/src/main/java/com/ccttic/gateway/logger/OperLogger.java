0a5f59b7d43b33102924ede2f06dbc007369e2d0
axzsd <atva725@qq.com>
2018-05-21 15:19:31 +0800

    wl xx

--- cqytjgpt-serivce-gateway/src/main/java/com/ccttic/gateway/logger/OperLogger.java
+++ cqytjgpt-serivce-gateway/src/main/java/com/ccttic/gateway/logger/OperLogger.java
@@ -1,11 +1,12 @@
 package com.ccttic.gateway.logger;
 
-import com.netflix.discovery.DiscoveryClient;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.Map;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
@@ -34,7 +35,20 @@ public class OperLogger extends ZuulFilter {
     public Object run() {
         // 获得当前请求的 request
         HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
-        System.out.println(request.getRequestURL());
+        if (request.getMethod().equalsIgnoreCase("POST")) {
+            System.out.println(getPostParams(request));
+        }
         return null;
+    }
+
+    /**
+     * 说明：获得HttpServletRequest POST的参数
+     * */
+    private Map<String, String[]> getPostParams(HttpServletRequest request) {
+        Map<String, String[]> map = null;
+        if (request.getMethod().equalsIgnoreCase("POST")) {
+            map = request.<String, String[]>getParameterMap();
+        }
+        return map;
     }
 }
