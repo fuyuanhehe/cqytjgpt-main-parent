e5a00a4612100edfad9a5f857261230f81aa0435
符源 <@LAPTOP-U0KHPA4D>
2018-05-30 11:34:03 +0800

    解决冲突

--- cqytjgpt-eureka-register/src/main/java/com/ccttic/register/RegistrationCenterApplication.java
+++ cqytjgpt-eureka-register/src/main/java/com/ccttic/register/RegistrationCenterApplication.java
@@ -8,8 +8,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistrationCenterApplication {
+	
+	
     public static void main(String [] args) {
         // 保留context
         ConfigurableApplicationContext context = new SpringApplicationBuilder(RegistrationCenterApplication.class).web(true).run(args);
     }
+    
+    
+    
+    
+    
 }
