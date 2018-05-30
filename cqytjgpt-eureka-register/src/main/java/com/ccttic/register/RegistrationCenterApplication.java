0362d6caf84346141ae677d741336acf6d3177bd
符源 <@LAPTOP-U0KHPA4D>
2018-05-30 11:34:44 +0800

    解决冲突

--- cqytjgpt-eureka-register/src/main/java/com/ccttic/register/RegistrationCenterApplication.java
+++ cqytjgpt-eureka-register/src/main/java/com/ccttic/register/RegistrationCenterApplication.java
@@ -1,22 +1,13 @@
 package com.ccttic.register;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistrationCenterApplication {
-	
-	
     public static void main(String [] args) {
-        // 保留context
-        ConfigurableApplicationContext context = new SpringApplicationBuilder(RegistrationCenterApplication.class).web(true).run(args);
+    	 SpringApplication.run(RegistrationCenterApplication.class, args);
     }
-    
-    
-    
-    
-    
 }
