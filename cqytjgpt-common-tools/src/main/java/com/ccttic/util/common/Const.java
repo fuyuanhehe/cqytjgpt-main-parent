c30e33dcc5ea1ac54d72070c1eeabe1f07a995a2
shuaiyao <Administrator@syao>
2018-06-04 10:17:11 +0800

    常量工具类

---
+++ cqytjgpt-common-tools/src/main/java/com/ccttic/util/common/Const.java
@@ -0,0 +1,33 @@
+  
+    /**    
+    * @Title: Const.java  
+    * @Package com.ccttic.util.common  
+    * @Description: TODO(用一句话描述该文件做什么)  
+    * @author Administrator  
+    * @date 2018年6月4日  
+    * @version V1.0    
+    */  
+    
+package com.ccttic.util.common;
+
+  
+    /**  
+    * @ClassName: Const  
+    * @Description: 常量类，系统中所有的常量都定义在里面
+    * @author syao  
+    * @date 2018年6月4日  
+    *    
+    */
+
+public class Const {
+	
+	/**
+	 * 从sesssion中获取用户的的key
+	 */
+	final public static String USER = "USER";
+	
+	/**
+	 * 从session中获取登录验证面的key
+	 */
+	final public static String PIC_CODE = "PIC_CODE";
+}
