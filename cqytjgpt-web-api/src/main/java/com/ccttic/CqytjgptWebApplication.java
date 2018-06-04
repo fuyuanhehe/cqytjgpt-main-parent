4859358664d949063634b6b70cb674c047bf7e8e
zhuliangmix <404582385@qq.com>
2018-06-04 14:40:16 +0800

    fastdfs文件系统支持

--- cqytjgpt-web-api/src/main/java/com/ccttic/CqytjgptWebApplication.java
+++ cqytjgpt-web-api/src/main/java/com/ccttic/CqytjgptWebApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
