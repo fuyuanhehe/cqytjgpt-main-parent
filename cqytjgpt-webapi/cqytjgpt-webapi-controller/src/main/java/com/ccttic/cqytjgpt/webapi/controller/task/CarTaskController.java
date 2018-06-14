3fb1f17cd66b1f2ff74ce43364189dc297ac2cbc
zhy <zhy>
2018-06-14 19:10:07 +0800

    修正路径

--- cqytjgpt-webapi/cqytjgpt-webapi-controller/src/main/java/com/ccttic/cqytjgpt/webapi/controller/task/CarTaskController.java
+++ cqytjgpt-webapi/cqytjgpt-webapi-controller/src/main/java/com/ccttic/cqytjgpt/webapi/controller/task/CarTaskController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ccttic.cqytjgpt.webapi.interfaces.taskcar.ITaskCarService;
 
 @RestController
-@RequestMapping("/carSerivce")
+@RequestMapping("/carService")
 public class CarTaskController{
 	
 	private ITaskCarService service;
