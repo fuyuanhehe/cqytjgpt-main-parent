0ceee20e3880a61831b98530cdf8db25d2fa3b74
zhuliangmix <404582385@qq.com>
2018-06-15 09:32:53 +0800

    删除原有项目目录结构

--- cqytjgpt-web-api/src/main/java/com/ccttic/service/taskcar/ITaskCarService.java
+++
@@ -1,8 +0,0 @@
-package com.ccttic.service.taskcar;
-
-public interface ITaskCarService {
-
-	void addCarIllega() throws Exception;
-	
-	void addCarDanger()throws Exception;
-}
--- cqytjgpt-web-api/src/main/java/com/ccttic/service/taskdriver/ITaskDriverService.java
+++
@@ -1,9 +0,0 @@
-package com.ccttic.service.taskdriver;
-
-public interface ITaskDriverService {
-	
-	void addDriveIllega()  throws Exception;
-	
-	void addDriverDanger()  throws Exception;
-
-}
