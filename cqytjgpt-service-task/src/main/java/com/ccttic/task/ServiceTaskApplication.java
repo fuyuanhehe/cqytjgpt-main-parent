7e72df0bcff7422b947c08936a0a1d3a77d0992c
zhuliangmix <404582385@qq.com>
2018-06-19 16:11:48 +0800

    定时任务结构修改

--- cqytjgpt-service-task/src/main/java/com/ccttic/task/ServiceTaskApplication.java
+++ cqytjgpt-service-task/src/main/java/com/ccttic/task/ServiceTaskApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ImportResource;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
+@ImportResource(locations = {"classpath:quartz-triggers.xml"})
 public class ServiceTaskApplication {
     public static void main(String [] args) {
         SpringApplication.run(ServiceTaskApplication.class, args);
--- cqytjgpt-service-task/src/main/java/com/ccttic/task/execute/DriverDangerTask.java
+++ cqytjgpt-service-task/src/main/java/com/ccttic/task/execute/DriverDangerTask.java
@@ -1,34 +1,27 @@
-package com.ccttic.task.taskexecute;
+package com.ccttic.task.execute;
 
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.PersistJobDataAfterExecution;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
 
 import com.ccttic.task.feign.IDriverServiceFeign;
-import com.ccttic.task.runer.AbstractTask;
 
 @Component
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
-public class DriverDangerTask extends AbstractTask{
+public class DriverDangerTask extends QuartzJobBean{
 	
 	@Autowired
 	private IDriverServiceFeign  feign;
 	
-	//启动时间
-	@Override
-	public String getCronStr() {
-		return "30 * * * * ?";
-	}
 	//执行方法
 	@Override
-	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
-
+	protected void executeInternal(JobExecutionContext arg) throws JobExecutionException {
 		feign.addDriveIllega();
 		feign.addDriverDanger();
 	}
-
 }
