cffc22a1f54e5ab3c4308a359cb40ecd42ca4a3e
zhy <zhy>
2018-06-21 09:27:13 +0800

    添加vo

---
+++ cqytjgpt-all-domain/src/main/java/com/ccttic/entity/drivers/vo/DriverIllegal.java
@@ -0,0 +1,45 @@
+package com.ccttic.entity.drivers.vo;
+
+import java.util.List;
+
+import com.ccttic.entity.drivers.Driver;
+import com.ccttic.entity.illegal.DrIllicit;
+
+public class DriverIllegal extends Driver {
+
+      private List<DrIllicit> drIllicits;
+
+	public List<DrIllicit> getDrIllicits() {
+		return drIllicits;
+	}
+
+	public void setDrIllicits(List<DrIllicit> drIllicits) {
+		this.drIllicits = drIllicits;
+	}
+
+	public DriverIllegal(String id, String name, String idcard, String telphone, String mobilephone, String adress,
+			String permicar, String firstrecivetime, String mgrdepart, String mgrdepartareaid, String mgrenterpriseid,
+			String nextexaminetime, String effectstarttime, String effectendtime, String examineeffectendtime,
+			String scoretotal, String state, List<DrIllicit> drIllicits) {
+		super(id, name, idcard, telphone, mobilephone, adress, permicar, firstrecivetime, mgrdepart, mgrdepartareaid,
+				mgrenterpriseid, nextexaminetime, effectstarttime, effectendtime, examineeffectendtime, scoretotal,
+				state);
+		this.drIllicits = drIllicits;
+	}
+
+	public DriverIllegal() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DriverIllegal(String id, String name, String idcard, String telphone, String mobilephone, String adress,
+			String permicar, String firstrecivetime, String mgrdepart, String mgrdepartareaid, String mgrenterpriseid,
+			String nextexaminetime, String effectstarttime, String effectendtime, String examineeffectendtime,
+			String scoretotal, String state) {
+		super(id, name, idcard, telphone, mobilephone, adress, permicar, firstrecivetime, mgrdepart, mgrdepartareaid,
+				mgrenterpriseid, nextexaminetime, effectstarttime, effectendtime, examineeffectendtime, scoretotal, state);
+		// TODO Auto-generated constructor stub
+	}
+      
+      
+}
---
+++ cqytjgpt-all-domain/src/main/java/com/ccttic/entity/role/vo/VehicleIllegal.java
@@ -0,0 +1,46 @@
+package com.ccttic.entity.role.vo;
+
+import java.util.List;
+
+import com.ccttic.entity.illegal.VehiIllicit;
+import com.ccttic.entity.role.Vehicle;
+
+public class VehicleIllegal extends Vehicle {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private List<VehiIllicit> vehiIllicits;
+
+	public List<VehiIllicit> getVehiIllicits() {
+		return vehiIllicits;
+	}
+
+	public void setVehiIllicits(List<VehiIllicit> vehiIllicits) {
+		this.vehiIllicits = vehiIllicits;
+	}
+
+	public VehicleIllegal(List<VehiIllicit> vehiIllicits) {
+		super();
+		this.vehiIllicits = vehiIllicits;
+	}
+
+	public VehicleIllegal() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public VehicleIllegal(String vehiNo, String vehiNoType, String vehiType, String nature, String ownership,
+			String owner, String telphone, String mobilephone, String adress, String permitCar, String firstReciveTime,
+			String mgrDepart, String mgrDepartAreaId, String areaNm, String mgrEnterpriseId, String etpNm,
+			String nextExamineTime, String effectStartTime, String effectEndTime, String examineEffectEndTime,
+			String scoreTotal, String state, String remark, String startTime, String endTime) {
+		super(vehiNo, vehiNoType, vehiType, nature, ownership, owner, telphone, mobilephone, adress, permitCar, firstReciveTime,
+				mgrDepart, mgrDepartAreaId, areaNm, mgrEnterpriseId, etpNm, nextExamineTime, effectStartTime, effectEndTime,
+				examineEffectEndTime, scoreTotal, state, remark, startTime, endTime);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
