65998669677b722ca71af37d16ca172c84416ec7
tangqian <403554123@qq.com>
2018-06-28 10:14:12 +0800

    a

--- cqytjgpt-all-domain/src/main/java/com/ccttic/entity/employee/vo/TokenVo.java
+++ cqytjgpt-all-domain/src/main/java/com/ccttic/entity/employee/vo/TokenVo.java
@@ -3,15 +3,18 @@ package com.ccttic.entity.employee.vo;
 public class TokenVo {
 	private String refreshToken;
 	private String axjToken;
+	private String access_token;
 	public TokenVo() {
 		super();
 	}
-	
-	public TokenVo(String refreshToken, String axjToken) {
+	public TokenVo(String refreshToken, String axjToken, String access_token) {
 		super();
 		this.refreshToken = refreshToken;
 		this.axjToken = axjToken;
+		this.access_token = access_token;
 	}
+
+
 
 	public String getRefreshToken() {
 		return refreshToken;
@@ -24,6 +27,12 @@ public class TokenVo {
 	}
 	public void setAxjToken(String axjToken) {
 		this.axjToken = axjToken;
+	}
+	public String getAccess_token() {
+		return access_token;
+	}
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
 	}
 	
 }
