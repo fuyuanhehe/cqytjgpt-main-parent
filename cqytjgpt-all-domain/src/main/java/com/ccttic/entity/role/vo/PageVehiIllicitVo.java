901d46c9612c9e72f01db4a2aec4b7cdf3d9b02b
zhy <zhy@DESKTOP-8K0HT35>
2018-07-16 09:39:10 +0800

    修改车辆违法bug

--- cqytjgpt-all-domain/src/main/java/com/ccttic/entity/role/vo/PageVehiIllicitVo.java
+++ cqytjgpt-all-domain/src/main/java/com/ccttic/entity/role/vo/PageVehiIllicitVo.java
@@ -2,32 +2,40 @@ package com.ccttic.entity.role.vo;
 
 import com.ccttic.entity.role.VehiIllicit;
 
-public class PageVehiIllicitVo extends VehiIllicit{
+public class PageVehiIllicitVo extends VehiIllicit {
 	private static final long serialVersionUID = -5437595901819895593L;
 	private int page = 1;
 	private int rows = 10;
 	private String mgrEnterpriseId;
-	
+	private String areaNm;
+
 	public PageVehiIllicitVo() {
-		super();
 	}
-	
-	public PageVehiIllicitVo(int page, int rows, String mgrEnterpriseId) {
-		super();
-		this.page = page;
-		this.rows = rows;
-		this.mgrEnterpriseId = mgrEnterpriseId;
+
+	public PageVehiIllicitVo(String vehiNo, String vehiNoType, String vehiNoTypeText, String nature, String ownership, String owner, String adress, String mgrDepart, String mgrDepartAreaId, String vehicleId, String illicitTime, String illicitScore, String illicitAmount, String illicitAdress, String illicit, String illicitDesc, String pickDepartment, String pickDepartmentDesc, String state, String remark, String etpNm, String startTime, String endTime) {
+		super(vehiNo, vehiNoType, vehiNoTypeText, nature, ownership, owner, adress, mgrDepart, mgrDepartAreaId, vehicleId, illicitTime, illicitScore, illicitAmount, illicitAdress, illicit, illicitDesc, pickDepartment, pickDepartmentDesc, state, remark, etpNm, startTime, endTime);
+	}
+
+	public String getAreaNm() {
+		return areaNm;
+	}
+
+	public void setAreaNm(String areaNm) {
+		this.areaNm = areaNm;
 	}
 
 	public int getPage() {
 		return page;
 	}
+
 	public void setPage(int page) {
 		this.page = page;
 	}
+
 	public int getRows() {
 		return rows;
 	}
+
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
@@ -39,5 +47,5 @@ public class PageVehiIllicitVo extends VehiIllicit{
 	public void setMgrEnterpriseId(String mgrEnterpriseId) {
 		this.mgrEnterpriseId = mgrEnterpriseId;
 	}
-	
+
 }
