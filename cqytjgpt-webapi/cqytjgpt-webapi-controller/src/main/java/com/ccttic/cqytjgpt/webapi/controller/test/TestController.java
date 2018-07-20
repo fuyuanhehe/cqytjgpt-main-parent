01858327ec6b7fe6b5e3625d5d49bd54c68ce32b
tangqian <403554123@qq.com>
2018-07-20 17:57:52 +0800

    a

--- cqytjgpt-webapi/cqytjgpt-webapi-controller/src/main/java/com/ccttic/cqytjgpt/webapi/controller/test/TestController.java
+++ cqytjgpt-webapi/cqytjgpt-webapi-controller/src/main/java/com/ccttic/cqytjgpt/webapi/controller/test/TestController.java
@@ -27,121 +27,121 @@ public class TestController implements Serializable {
 	@ResponseBody
 	public void testSurveil() {
 		List<Vehicle> vehicle = testIllicitService.qryVehicleList();
-		for (Vehicle ve : vehicle) {
-			List<NetTrffSurveil> surveilList = testIllicitService.qryNetTrffSurveilList("渝"+ve.getVehiNo());
-			if (surveilList.size()>0) {
-				for (NetTrffSurveil net : surveilList) {
-					String uuid = RandomHelper.uuid();
-					VehiDrIllicit veDr = new VehiDrIllicit();
-					veDr.setId(uuid);
-					// 违法记分IllicitScore
-					if (null != net.getCLBJ()) { 
-						veDr.setDisposeSign(net.getCLBJ());
-					}
-					if (null != net.getJDSBH()) {
-						veDr.setDecisionNumber(net.getJDSBH());
-					}
-					if (null != net.getWFSJ()) {
-						veDr.setIllicitTime(net.getWFSJ());
-					}
-					if (null != net.getFKJE()) {
-						veDr.setIllicitAmount(net.getFKJE());
-					}
-					if (null != net.getWFDZ()) {
-						veDr.setIllicitAdress(net.getWFDZ());
-					}
-					if (null != net.getWFXW()) {
-						veDr.setIllicit(net.getWFXW());
-					}
-					if (null != net.getXH()) {
-						veDr.setSerialNumber(net.getXH());
-					}
-//					veDr.setIllicitDesc("");// TODO 违法行为描述
-					if (null != net.getCJJG()) {
-						veDr.setPickDepartment(net.getCJJG());
-					}
-					if (null != net.getCJJGMC()) {
-						veDr.setPickDepartmentDesc(net.getCJJGMC());
-					}
-					veDr.setIllicitTime(net.getWFSJ());
-					
-					// 是否是机动车违法
-					if (net.getCLBJ().equals("0")) { 
-						veDr.setVehiIllicitState("1");
-						veDr.setDrIllicitState("0");
-						veDr.setVehiState("001");
-					} else if (net.getCLBJ().equals("1") && net.getJKBJ().equals("0")){
-						veDr.setVehiIllicitState("1");
-						veDr.setDrIllicitState("1");
-						veDr.setVehiState("002");
-					} else if (net.getCLBJ().equals("1") && (net.getJKBJ().equals("1")||net.getJKBJ().equals("9"))) {
-						veDr.setVehiIllicitState("1");
-						veDr.setDrIllicitState("1");
-						veDr.setVehiState("003");
-					}
-					// 是否是驾驶人违法
-//					if (net.getCLBJ().equals("1")&&(net.getJKBJ().equals("1")||net.getJKBJ().equals("9"))) {
-//						veDr.setDrIllicitState("1");
-//					} else {
-//						veDr.setDrIllicitState("0");
+//		for (Vehicle ve : vehicle) {
+//			List<NetTrffSurveil> surveilList = testIllicitService.qryNetTrffSurveilList("渝"+ve.getVehiNo());
+//			if (surveilList.size()>0) {
+//				for (NetTrffSurveil net : surveilList) {
+//					String uuid = RandomHelper.uuid();
+//					VehiDrIllicit veDr = new VehiDrIllicit();
+//					veDr.setId(uuid);
+//					// 违法记分IllicitScore
+//					if (null != net.getCLBJ()) { 
+//						veDr.setDisposeSign(net.getCLBJ());
 //					}
-					veDr.setVehiNo(ve.getVehiNo());
-					if (null != ve.getVehiType()) {
-						veDr.setVehiNoType(ve.getVehiType());
-					}
-					if (null != ve.getNature()) {
-						veDr.setNature(ve.getNature());
-					}
-					if (null != ve.getOwnership()) {
-						veDr.setOwnership(ve.getOwnership());
-					}
-					if (null != ve.getOwner()) {
-						veDr.setOwner(ve.getOwner());
-					}
-					if (null != ve.getAdress()) {
-						veDr.setAdress(ve.getAdress());
-					}
-					if (null != ve.getMgrDepartAreaId()) {
-						veDr.setMgrDepartAreaId(ve.getMgrDepartAreaId());
-					}
-					veDr.setVehicleId(ve.getId());
-					// 根据年份入库
-					String WFSJdate = net.getWFSJ().substring(0, 4);
-					String table = "vehi_dr_illicit";
-					if (WFSJdate.equals("2009")) {
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2010")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2011")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2012")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2013")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2014")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2015")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2016")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2017")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					} else if (WFSJdate.equals("2018")){
-						veDr.setTableName(table+WFSJdate);
-						testIllicitService.addIllicit(veDr);
-					}
-				}
-			}
-		}
+//					if (null != net.getJDSBH()) {
+//						veDr.setDecisionNumber(net.getJDSBH());
+//					}
+//					if (null != net.getWFSJ()) {
+//						veDr.setIllicitTime(net.getWFSJ());
+//					}
+//					if (null != net.getFKJE()) {
+//						veDr.setIllicitAmount(net.getFKJE());
+//					}
+//					if (null != net.getWFDZ()) {
+//						veDr.setIllicitAdress(net.getWFDZ());
+//					}
+//					if (null != net.getWFXW()) {
+//						veDr.setIllicit(net.getWFXW());
+//					}
+//					if (null != net.getXH()) {
+//						veDr.setSerialNumber(net.getXH());
+//					}
+////					veDr.setIllicitDesc("");// TODO 违法行为描述
+//					if (null != net.getCJJG()) {
+//						veDr.setPickDepartment(net.getCJJG());
+//					}
+//					if (null != net.getCJJGMC()) {
+//						veDr.setPickDepartmentDesc(net.getCJJGMC());
+//					}
+//					veDr.setIllicitTime(net.getWFSJ());
+//					
+//					// 是否是机动车违法
+//					if (net.getCLBJ().equals("0")) { 
+//						veDr.setVehiIllicitState("1");
+//						veDr.setDrIllicitState("0");
+//						veDr.setVehiState("001");
+//					} else if (net.getCLBJ().equals("1") && net.getJKBJ().equals("0")){
+//						veDr.setVehiIllicitState("1");
+//						veDr.setDrIllicitState("1");
+//						veDr.setVehiState("002");
+//					} else if (net.getCLBJ().equals("1") && (net.getJKBJ().equals("1")||net.getJKBJ().equals("9"))) {
+//						veDr.setVehiIllicitState("1");
+//						veDr.setDrIllicitState("1");
+//						veDr.setVehiState("003");
+//					}
+//					// 是否是驾驶人违法
+////					if (net.getCLBJ().equals("1")&&(net.getJKBJ().equals("1")||net.getJKBJ().equals("9"))) {
+////						veDr.setDrIllicitState("1");
+////					} else {
+////						veDr.setDrIllicitState("0");
+////					}
+//					veDr.setVehiNo(ve.getVehiNo());
+//					if (null != ve.getVehiType()) {
+//						veDr.setVehiNoType(ve.getVehiType());
+//					}
+//					if (null != ve.getNature()) {
+//						veDr.setNature(ve.getNature());
+//					}
+//					if (null != ve.getOwnership()) {
+//						veDr.setOwnership(ve.getOwnership());
+//					}
+//					if (null != ve.getOwner()) {
+//						veDr.setOwner(ve.getOwner());
+//					}
+//					if (null != ve.getAdress()) {
+//						veDr.setAdress(ve.getAdress());
+//					}
+//					if (null != ve.getMgrDepartAreaId()) {
+//						veDr.setMgrDepartAreaId(ve.getMgrDepartAreaId());
+//					}
+//					veDr.setVehicleId(ve.getId());
+//					// 根据年份入库
+//					String WFSJdate = net.getWFSJ().substring(0, 4);
+//					String table = "vehi_dr_illicit";
+//					if (WFSJdate.equals("2009")) {
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2010")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2011")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2012")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2013")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2014")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2015")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2016")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2017")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					} else if (WFSJdate.equals("2018")){
+//						veDr.setTableName(table+WFSJdate);
+//						testIllicitService.addIllicit(veDr);
+//					}
+//				}
+//			}
+//		}
 		for (Vehicle ve2 : vehicle) {
 			List<NetTrffViolation> tion = testIllicitService.qryNetTrffViolationList("渝"+ve2.getVehiNo());
 			if (tion.size()>0) {
@@ -167,6 +167,14 @@ public class TestController implements Serializable {
 					if (null != ve2.getMgrDepartAreaId()) {
 						veDr.setMgrDepartAreaId(ve2.getMgrDepartAreaId());
 					}
+					if (net.getJKBJ().equals("0")) {
+						veDr.setDisposeSign("0");
+					} else {
+						veDr.setDisposeSign("1");
+					}
+					
+					
+					
 					veDr.setVehicleId(ve2.getId());
 					
 					if (null != net.getJDSBH()) {
