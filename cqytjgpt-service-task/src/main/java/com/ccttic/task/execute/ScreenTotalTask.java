80111fc9b4c3e320dc2f48531dc1793016237899
zzy <1203508644@qq.com>
2018-07-31 09:13:17 +0800

    测试定时任务打印

--- cqytjgpt-service-task/src/main/java/com/ccttic/task/execute/ScreenTotalTask.java
+++ cqytjgpt-service-task/src/main/java/com/ccttic/task/execute/ScreenTotalTask.java
@@ -6,6 +6,8 @@ import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.PersistJobDataAfterExecution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
@@ -14,11 +16,13 @@ import org.springframework.stereotype.Component;
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
 public class ScreenTotalTask extends QuartzJobBean {
+    private Logger log = LoggerFactory.getLogger(ScreenTotalTask.class);
     @Autowired
     private ISceenTotalFeign feign;
     //执行方法
     @Override
     protected void executeInternal(JobExecutionContext arg) throws JobExecutionException {
+        log.info("定时任务开始");
         feign.screenTotal();
     }
 }
